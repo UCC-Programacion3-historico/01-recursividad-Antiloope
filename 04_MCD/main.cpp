988f5e84c4aa06465cbc8dca28fd3e621df18312
Antiloope <rodrigopizarro1234@gmail.com>
2017-08-16 10:55:48 -0300

    Ejercicio 04 mcd Terminado - Guia 01 - Prg3

--- 04_MCD/main.cpp
+++ 04_MCD/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include "mcd.h"
 
+using namespace std;
+
 int main() {
-    std::cout << "Ejercicio 01/04\n" << std::endl;
+    unsigned int a,b;
+    cout << "Ingrese a: " << '\n';
+    cin >> a;
+    cout << "Ingrese b: " << '\n';
+    cin >> b;
+    cout << "El maximo comun divisor es: " << mcd(a,b) << '\n';
+    cout << "Ejercicio 01/04\n" << std::endl;
     return 0;
 }
