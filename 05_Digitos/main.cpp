caed7adbee14752cf6361219dc8e147329a9ad19
Antiloope <rodrigopizarro1234@gmail.com>
2017-08-16 11:24:04 -0300

    Ejercicio 05 Digitos Termimnado - Guia 01 - Prg3

--- 05_Digitos/main.cpp
+++ 05_Digitos/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include "digitos.h"
 
+using namespace std;
+
 int main() {
-    std::cout << "Ejercicio 01/05\n" << std::endl;
+    unsigned int a,b;
+    cout << "Ingrese a: " << '\n';
+    cin >> a;
+    cout << "Ingrese b: " << '\n';
+    cin >> b;
+    cout << "Cociente entre a y b: " << cociente(a,b) << '\n';
+    cout << "Resto entre a y b: " << resto(a,b) << '\n';
+    cout << "Ejercicio 01/05\n" << endl;
     return 0;
 }
