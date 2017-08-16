41f409439a6e7c3a0ed13ec6bdb72a0db2c4393b
Antiloope <rodrigopizarro1234@gmail.com>
2017-08-16 10:45:05 -0300

    Ejercicio 03 Aackerman Terminado - Guia 01 - Prg3

--- 03_Ackerman/main.cpp
+++ 03_Ackerman/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include "ackerman.h"
 
+using namespace std;
+
 int main() {
-    std::cout << "Ejercicio 01/03\n" << std::endl;
+    unsigned int m,n;
+    cout << "Ingrese la cantidad de elementos: " << '\n';
+    cin >> m;
+    cout << "Ingrese de a cuantos: " << '\n';
+    cin >> n;
+    cout << "La combinacion es: " << ackerman(m,n) << '\n';
+    cout << "Ejercicio 01/03\n" << std::endl;
     return 0;
 }
